aaac2d94b96354e5ac8eb9d01f8ee2a8f73dca58
KazuhiraBenedictMiller <andrea.amedeo.serravalle@protonmail.com>
2023-10-05 13:30:11 +0200

    Streamlit ChatApp Model Checkpoint

--- source/config.py
+++ source/config.py
@@ -10,3 +10,5 @@ PINECONE_API_TOKEN = os.environ["PINECONE_API_TOKEN"]
 
 PINECONE_ENVIRONMENT = "gcp-starter"
 PINECONE_INDEX_NAME = "centralbanksllm"
+
+LLAMA2_13B = "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5"
