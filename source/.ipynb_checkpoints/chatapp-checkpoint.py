e8029fd4caa0dbfa14db3747d4d7902c6faf5ca2
KazuhiraBenedictMiller <andrea.amedeo.serravalle@protonmail.com>
2023-10-10 20:40:00 +0200

    Streamlit ChatApp Model Checkpoint

--- source/.ipynb_checkpoints/chatapp-checkpoint.py
+++ source/.ipynb_checkpoints/chatapp-checkpoint.py
@@ -232,7 +232,7 @@ if "FetchingPhase" not in st.session_state.keys() or st.session_state["FetchingP
         
         ProgressBar.progress(20, "Fetching Data")
         
-        print(st.session_state["StartYear"], st.session_state["EndYear"])
+        st.text(st.session_state["StartYear"], st.session_state["EndYear"])
         
         ETL_Pipeline(st.session_state["StartYear"], st.session_state["EndYear"])
         
