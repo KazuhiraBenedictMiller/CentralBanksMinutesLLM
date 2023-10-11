3e19bba64e0a47e15eeead0b4250967d4612281d
KazuhiraBenedictMiller <andrea.amedeo.serravalle@protonmail.com>
2023-10-11 10:27:00 +0200

    Streamlit ChatApp Model Checkpoint

--- source/.ipynb_checkpoints/chatapp-checkpoint.py
+++ source/.ipynb_checkpoints/chatapp-checkpoint.py
@@ -235,7 +235,7 @@ if "FetchingPhase" not in st.session_state.keys() or st.session_state["FetchingP
         st.text(type(st.session_state["StartYear"]))
         st.text(type(st.session_state["EndYear"]))
         
-        ETL_Pipeline(2006, 2006)
+        ETL_Pipeline(int(st.session_state["StartYear"]), int(st.session_state["EndYear"]))
         
         #Function to Fetch Data, Generate Embeddings and Load them into VectorStore
 
